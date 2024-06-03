13df29b039cc0f8ac181a17780520aa72ca7b12a
user1 <user1@userdomain.com>
2024-06-03 16:52:20 -0300

    Add - Contact header and first row

--- contactenos.php
+++ contactenos.php
@@ -1,20 +1,45 @@
 <?php
 
-$PageTitle="Helvetia - Contáctenos";
+$PageTitle = "Helvetia - Contáctenos";
 $page = "contactenos";
 
-function customPageHeader(){?>
+function customPageHeader()
+{ ?>
   <!--Arbitrary HTML Tags-->
 <?php }
 
-include_once('templates/header.php'); ?>
+include_once ('templates/header.php'); ?>
 
-<div class="container vh-100">
-  <div class="row py-4">
-    <h1>Helvetia - Contáctenos</h1>
+<div class="container-fluid">
+  <div class="row header-bg-img py-xl-5 py-3">
+    <h1 class="text-center text-white">Contáctanos</h1>
+  </div>
+</div>
+
+<div class="container py-5 mio-bg-gradient">
+  <div class="row gap-3 d-flex">
+
+        <div class="col d-flex flex-column align-items-center gap-2 border rounded-3 p-4">
+          <i class="fa-solid fa-location-dot fs-3 text-primary"></i>
+          <h2 class="fs-6 text-primary mio-bg-gradient">Dirección</h2>
+          <p class="text-center">Via 11 Esq. Via Viviana, Centro Comercial Villa Fontana II, Carolina, PR 00983</p>
+        </div>
+
+      <div class="col d-flex flex-column align-items-center gap-2 border rounded-3 p-4">
+        <i class="fa-solid fa-phone fs-3 text-primary"></i>
+        <h2 class="fs-6 text-primary mio-bg-gradient">Teléfono</h2>
+        <p class="text-center">(787)-750-3300</p>
+      </div>
+
+      <div class="col d-flex flex-column align-items-center gap-2 border rounded-3 p-4">
+        <i class="fa-solid fa-envelope fs-3 text-primary"></i>
+        <h2 class="fs-6 text-primary mio-bg-gradient">Correo</h2>
+        <p class="text-center">info@helvetia-pr.com</p>
+      </div>
+
   </div>
 </div>
 
 <?php
-include_once('templates/footer.php');
+include_once ('templates/footer.php');
 ?>
