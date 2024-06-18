4595de28a9a195162d9a6ae79195405bee20a311
user1 <user1@userdomain.com>
2024-06-18 10:23:42 -0300

    Fix - APIS and Beneficios Templates Responsive

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,7 @@ $(window).ready(function () {
 
 
 // Carousel
-let items = document.querySelectorAll('.carousel .carousel-item')
+/*let items = document.querySelectorAll('.carousel .carousel-item')
 
 		items.forEach((el) => {
 			const minPerSlide = 4
@@ -23,4 +23,36 @@ let items = document.querySelectorAll('.carousel .carousel-item')
         el.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
     }
-})
+})*/
+
+var multipleCardCarousel = document.querySelector(
+  "#carouselExampleControls"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+  });
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
+  $("#carouselExampleControls .carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 5) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide");
+}
--- templates/beneficios.php
+++ templates/beneficios.php
@@ -4,26 +4,34 @@
     <div class="row text-center py-5">
       <h2 class="text-primary fw-bold">Proveemos los mejores beneficios</h2>
     </div>
-    <div class="row gap-4">
-      <div class="col p-4 bg-white rounded-3">
-        <img class="mb-3" src="<?php $SERVER_URI ?>images/experiencia.svg" alt="">
-        <h3 class="fs-6 text-primary text-bold">Experiencia</h3>
-        <p>Tenemos una amplia experiencia en el mercado. 33 años sirviendo de manera exclusiva al mercado de ahorro y crédito.</p>
+    <div class="row">
+      <div class="col-12 col-md-3">
+        <div class="p-4 bg-white rounded-3 h-100">
+          <img class="mb-3" src="<?php $SERVER_URI ?>images/experiencia.svg" alt="">
+          <h3 class="fs-6 text-primary text-bold">Experiencia</h3>
+          <p>Tenemos una amplia experiencia en el mercado. 33 años sirviendo de manera exclusiva al mercado de ahorro y crédito.</p>
+        </div>
       </div>
-      <div class="col p-4 bg-white rounded-3">
-        <img class="mb-3" src="<?php $SERVER_URI ?>images/cero-desventajas.svg" alt="">
-        <h3 class="fs-6 text-primary text-bold">Cero Desventajas</h3>
-        <p>Único proveedor para las cooperativas con un centro de llamadas de servicio al cliente en Puerto Rico.</p>
+      <div class="col-12 col-md-3">
+        <div class="p-4 bg-white rounded-3 h-100">
+          <img class="mb-3" src="<?php $SERVER_URI ?>images/cero-desventajas.svg" alt="">
+          <h3 class="fs-6 text-primary text-bold">Cero Desventajas</h3>
+          <p>Único proveedor para las cooperativas con un centro de llamadas de servicio al cliente en Puerto Rico.</p>
+        </div>
       </div>
-      <div class="col p-4 bg-white rounded-3">
-        <img class="mb-3" src="<?php $SERVER_URI ?>images/unico-proveedor.svg" alt="">
-        <h3 class="fs-6 text-primary text-bold">Único Proveedor</h3>
-        <p>Única institución de servicios que se ha mantenido desarrollando y educando al personal.</p>
+      <div class="col-12 col-md-3">
+        <div class="p-4 bg-white rounded-3 h-100">
+          <img class="mb-3" src="<?php $SERVER_URI ?>images/unico-proveedor.svg" alt="">
+          <h3 class="fs-6 text-primary text-bold">Único Proveedor</h3>
+          <p>Única institución de servicios que se ha mantenido desarrollando y educando al personal.</p>
+        </div>
       </div>
-      <div class="col p-4 bg-white rounded-3">
-        <img class="mb-3" src="<?php $SERVER_URI ?>images/mantenimiento.svg" alt="">
-        <h3 class="fs-6 text-primary text-bold">Mantenimiento</h3>
-        <p>Hacer mejoras al sistema a través de conexión remota para agilizar los procesos de los clientes.</p>
+      <div class="col-12 col-md-3">
+        <div class="p-4 bg-white rounded-3 h-100">
+          <img class="mb-3" src="<?php $SERVER_URI ?>images/mantenimiento.svg" alt="">
+          <h3 class="fs-6 text-primary text-bold">Mantenimiento</h3>
+          <p>Hacer mejoras al sistema a través de conexión remota para agilizar los procesos de los clientes.</p>
+        </div>
       </div>
     </div>
   </div>
