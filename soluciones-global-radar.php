86dd162532fb8f8dc5429466571d15b7a2f141b2
Gabrieldeleon <gabriel.miopr@gmail.com>
2024-06-21 18:17:11 -0400

    - Continuación de trabajo sobre página de Global Radar

--- soluciones-global-radar.php
+++ soluciones-global-radar.php
@@ -9,9 +9,144 @@ function customPageHeader(){?>
 
 include_once('templates/header.php'); ?>
 
-<div class="container vh-100">
-  <div class="row py-4">
-    <h1>Soluciones - Global Radar</h>
+<div class="container-fluid">
+  <div class="row header-bg-img py-xl-5 py-3">
+    <h1 class="text-center text-white">Global RADAR®</h1>
+  </div>
+</div>
+
+<!-- Primer container -->
+<div class="container-lg mw-1120">
+  <div class="row py-100 d-flex gx-md-5 align-items-center">
+    <div class="col-sm-6">
+      <H1 class="fs-3 fw-bold text-primary pb-4">¿Quién es Global RADAR®</H1>
+      <p>Global RADAR® es una empresa líder con más de diez años de experiencia en servicios financieros, dedicada a ofrecer soluciones de cumplimiento y gestión de riesgos de clase mundial. Con presencia global en ocho ubicaciones estratégicas, nuestro equipo está comprometido en proporcionar programas de software de alta calidad que satisfacen las necesidades de una amplia base de clientes en más de veinte países.
+      </p>
+    </div>
+    <div class="col-sm-6">
+      <img class="rounded-4 img-fluid" src="images/temp-image.svg" alt="temp-image">
+    </div>
+  </div>
+</div>
+
+<!-- Segundo Container -->
+<div class="container-fluid py-100 gr-bg">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-8 mx-auto">
+        <h2 class="text-white">¿Qué Hacemos?</h2>
+        <p class="text-white mt-3">Helvetia del Caribe es una empresa que desarrolla e implementa soluciones informáticas, fundada en 1986 cuando se incorpora bajo las leyes del Estado Libre Asociado de Puerto Rico. Helvetia del Caribe tiene soluciones para el comercio, la industria, y para el desarrollo de aplicaciones. Helvetia del Caribe es una empresa netamente puertorriqueña. Nuestras oficinas están localizadas en Carolina, Puerto Rico y ocupan un espacio de aproximadamente 12,000 pies cuadrados.
+        </p>
+        <div class="card gr_card--mw text-start mx-auto mt-md-5">
+          <div class="row p-sm-2 p-md-3 align-items-center">
+            <div class="col-md-3">
+              <img class="img-fluid" src="images/phone.svg" alt="phone icon">
+            </div>
+            <div class="col-md-9 align-items-center">
+              <p class="fs-6 mb-1 light-gray-2">Soporte 24/7</p>
+              <p class="fs-6 fw-bold mb-0">
+                <a href="tel:(787) 990-9090">Llámanos: (787) 990-9090</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Tercer Container -->
+<div class="container py-100 mw-1120">
+  <div class="row text-center">
+    <div class="col mx-auto">
+      <h2 class="fs-3 fw-bold text-primary">Diferenciadores Clave</h2>
+      <div class="row gx-md-2 custom-card">
+        <div class="col-md-6">
+          <div class="card text-start mt-md-3">
+            <div class="row p-sm-2 p-md-3 align-items-center">
+              <div class="col-1 align-items-center">
+                <img class="checked-list" src="images/check-circle-fill.svg" alt=""/>
+              </div>
+              <div class="col-11 align-items-center">
+                <p class="mb-1 light-gray-2"><strong>Venta Cruzada en Tiempo Real:</strong> Utilice datos efectivos para impulsar ventas adicionales de productos y servicios.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="card text-start mt-md-3">
+            <div class="row p-sm-2 p-md-3 align-items-center">
+              <div class="col-1 align-items-center">
+                <img class="checked-list" src="images/check-circle-fill.svg" alt=""/>
+              </div>
+              <div class="col-11 align-items-center">
+                <p class="mb-1 light-gray-2"><strong>Pruebas en Red de Preparación:</strong> Asegure una implementación fluida con pruebas exhaustivas antes de producción.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="card text-start mt-md-3">
+            <div class="row p-sm-2 p-md-3 align-items-center">
+              <div class="col-1 align-items-center">
+                <img class="checked-list" src="images/check-circle-fill.svg" alt=""/>
+              </div>
+              <div class="col-11 align-items-center">
+                <p class="mb-1 light-gray-2"><strong>Proceso Acelerado de Cuentas:</strong> Facilita la apertura y revisión de cuentas de cumplimiento de manera eficiente.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="card text-start mt-md-3">
+            <div class="row p-sm-2 p-md-3 align-items-center">
+              <div class="col-1 align-items-center">
+                <img class="checked-list" src="images/check-circle-fill.svg" alt=""/>
+              </div>
+              <div class="col-11 align-items-center">
+                <p class="mb-1 light-gray-2"><strong>Auto-configuración:</strong> Empodere a su equipo con el control directo sobre la configuración del sistema.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card text-start mt-md-3">
+            <div class="row p-sm-2 p-md-3 align-items-center">
+              <div class="col-1 align-items-center">
+                <img class="checked-list" src="images/check-circle-fill.svg" alt=""/>
+              </div>
+              <div class="col-11 align-items-center">
+                <p class="mb-1 light-gray-2"><strong>Actualizaciones Continuas:</strong> Implemente mejoras rápidamente sin largos tiempos de espera.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="card text-start mt-md-3">
+            <div class="row p-sm-2 p-md-3 align-items-center">
+              <div class="col-1 align-items-center">
+                <img class="checked-list" src="images/check-circle-fill.svg" alt=""/>
+              </div>
+              <div class="col-11 align-items-center">
+                <p class="mb-1 light-gray-2"><strong>Análisis Avanzado de Relaciones:</strong> Proporciona perspectivas profundas sobre la organización y relaciones de los clientes más allá de los sistemas tradicionales.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="card text-start mt-md-3">
+            <div class="row p-sm-2 p-md-3 align-items-center">
+              <div class="col-1 align-items-center">
+                <img class="checked-list" src="images/check-circle-fill.svg" alt=""/>
+              </div>
+              <div class="col-11 align-items-center">
+                <p class="mb-1 light-gray-2"><strong>Modelo de Datos Sofisticado:</strong> Captura la complejidad de la estructura del cliente con gran detalle.
+                </p>
+              </div>
+            </div>
+          </div> 
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
