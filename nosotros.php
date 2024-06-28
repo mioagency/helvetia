c90a95c5aa40c9103be9a81e91ca2042812df313
Rudy Duran <dbruddy@gmail.com>
2024-06-28 17:39:27 -0400

    Página de Nosotros agregada

--- nosotros.php
+++ nosotros.php
@@ -1,19 +1,309 @@
 <?php
 
-$PageTitle="Helvetia - Nosotros";
-$page="nosotros";
+$PageTitle = "Soluciones - Global Radar";
+$page = "global radar";
 
-function customPageHeader(){?>
+function customPageHeader()
+{ ?>
   <!--Arbitrary HTML Tags-->
 <?php }
 
 include_once('templates/header.php'); ?>
 
-<div class="container vh-100">
-  <div class="row py-4">
-    <h1>Helvetia - Nosotros</h1>
+<div class="container-fluid">
+  <div class="row header-bg-img py-xl-5 py-3">
+    <h1 class="text-center text-white">Nosotros</h1>
   </div>
 </div>
+
+<div class="container-lg">
+  <div class="row pt-100 pb-64 d-flex mio-bg-gradient">
+    <div class="col-sm-4">
+      <h1 class="fs-3 fw-bold text-primary">¿Qué NCUS Web</h1>
+    </div>
+    <div class="col-sm-8">
+      <p>Helvetia del Caribe es una empresa que desarrolla e implementa soluciones informáticas, fundada en 1986
+        cuando se incorpora bajo las leyes del Estado Libre Asociado de Puerto Rico. Helvetia del Caribe tiene
+        soluciones para el comercio, la industria, y para el desarrollo de aplicaciones. Helvetia del Caribe es
+        una empresa netamente puertorriqueña. Nuestras oficinas están localizadas en Carolina, Puerto Rico y
+        ocupan un espacio de aproximadamente 12,000 pies cuadrados.
+      </p>
+      <p>
+        Su principal producto es NCUS© (Next generation Credit Union System©), un sistema de información
+        integrada para el manejo y administración de una cooperativa de ahorro y crédito, siendo nuestro
+        producto el más instalado y usado actualmente en cooperativas de ahorro y crédito en Puerto Rico. El
+        mismo está instalado en más de 60 cooperativas. Además, Helvetia del Caribe, Inc. representa otros
+        productos en Puerto Rico.
+      </p>
+      <p>
+        Helvetia del Caribe cuenta con más de 60 empleados para darle servicios a sus clientes, tanto en sus
+        oficinas, así como soporte remoto desde nuestro centro de servicio. En nuestras oficinas tenemos un
+        centro de cómputos con una gran variedad de equipo. También mantenemos un Departamento de investigación
+        y desarrollo con personal técnico dedicado a mejorar y mantener informados a nuestros clientes de los
+        últimos adelantos en sistema y tecnología.
+      </p>
+    </div>
+  </div>
+</div>
+
+<!-- Segundo Container -->
+<div class="container-fluid bg-light-secondary py-100">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col mx-auto">
+        <div class="row text-center">
+          <div class="col col-md-8 mx-auto mb-4">
+            <h2 class="fs-3 fw-bold text-primary  mb-4">¿Por qué Nosotros?</h2>
+            <p>En todo mercado existen variedad de soluciones. Así que la pregunta que corresponde es cuál
+              es la diferencia entre una y otra. Helvetia del Caribe tiene cualidades que la hacen
+              diferente a los demás. Por eso nuestro lema es Helvetia del Caribe es inversión segura.</p>
+          </div>
+        </div>
+
+        <div class="row gx-md-2 custom-card">
+          <!-- Tarjetas de diferenciadores -->
+          <div class="col-md-8 d-none d-sm-block mx-auto">
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Vasta experiencia en el mercado, sobre 30 años
+                    sirviendo de manera exclusiva al mercado de ahorro y crédito.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Sobre sesenta cooperativas en Puerto Rico operan con
+                    nuestros sistemas por más de una década.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Empresa con formalidad en sus instalaciones. No existe
+                    improvisación ni falta de experiencia. Sobre 2,500 instalaciones exitosas de
+                    productos durante mas de 30 años.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Contamos con 60 empleados motivados. Promediando cuatro
+                    empleos nuevos por año de existencia. No ha existido una empresa local dedicada
+                    a cooperativas que haya empleado tanto personal.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Capitalizamos nuestros ingresos. Única institución de
+                    servicios que se ha mantenido desarrollando, educando al personal e invirtiendo
+                    en
+                    infraestructura para tranquilidad de nuestros clientes.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Contamos con todos los productos necesarios para no
+                    estar en desventaja con la banca. Trabajan en tiempo real red, cajeros
+                    automáticos, TeleCoop, Home Banking y Shared Branch. Estos productos han sido
+                    desarrollados con la más alta tecnología.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Todos los productos son integrados para evitar doble
+                    esfuerzo e incompatibilidad entre los productos.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Contamos con todos los productos necesarios para la
+                    digitalización total de la cooperativa.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <!-- Movil : Columna -->
+          <div id="mobile-side-scroll" class="col d-sm-none">
+
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Vasta experiencia en el mercado, sobre 30 años
+                    sirviendo de manera exclusiva al mercado de ahorro y crédito.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Sobre sesenta cooperativas en Puerto Rico operan con
+                    nuestros sistemas por más de una década.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Empresa con formalidad en sus instalaciones. No existe
+                    improvisación ni falta de experiencia. Sobre 2,500 instalaciones exitosas de
+                    productos durante mas de 30 años.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Contamos con 60 empleados motivados. Promediando cuatro
+                    empleos nuevos por año de existencia. No ha existido una empresa local dedicada
+                    a cooperativas que haya empleado tanto personal.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Capitalizamos nuestros ingresos. Única institución de
+                    servicios que se ha mantenido desarrollando, educando al personal e invirtiendo
+                    en
+                    infraestructura para tranquilidad de nuestros clientes.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Contamos con todos los productos necesarios para no
+                    estar en desventaja con la banca. Trabajan en tiempo real red, cajeros
+                    automáticos, TeleCoop, Home Banking y Shared Branch. Estos productos han sido
+                    desarrollados con la más alta tecnología.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Todos los productos son integrados para evitar doble
+                    esfuerzo e incompatibilidad entre los productos.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-gray-2">Contamos con todos los productos necesarios para la
+                    digitalización total de la cooperativa.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <!-- Fin elementos móvil -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Row Compromiso -->
+<div class="container-lg mw-1120">
+  <div class="row py-100 d-flex gx-md-5 align-items-center">
+    <div class="col-sm-6">
+      <h2 class="fs-3 fw-bold text-primary pb-4">Nuestro Compromiso</h2>
+      <p>Ofrecer a nuestros clientes un servicio único en su clase. El mismo está basado en un paquete diseñado
+        para el desarrollo y mantenimiento de un sistema integrado de procesamiento de datos para cooperativas
+        de ahorro y crédito. La plena satisfacción plena de nuestros clientes es testimonio inequívoco de la
+        efectividad de nuestro sistema y la calidad de nuestro servicio.
+      </p>
+      <p>También ofrecemos las licencias del "software" de aplicación, del sistema operativo, la instalación y
+        configuración del sistema operativo, de sistemas de mensajería y colaboración, el equipo y la
+        infraestructura de redes.</p>
+    </div>
+    <div class="col-sm-6">
+      <img class="rounded-4 img-fluid" src="images/temp-image.svg" alt="temp-image">
+    </div>
+  </div>
+</div>
+
+<!-- Template Beneficios -->
+<?php include_once('templates/beneficios.php'); ?>
 
 <?php
 include_once('templates/footer.php');
