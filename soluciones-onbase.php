2545a189f49049f929bb9af70e5342dabafd600d
WebifyArg <mitrerod@gmail.com>
2024-11-14 22:07:30 -0300

    Title change - item descriptions deleted

--- soluciones-onbase.php
+++ soluciones-onbase.php
@@ -35,99 +35,138 @@ include_once('templates/header.php'); ?>
 <div class="container-md mw-930 py-4 py-100">
     <div class="row">
         <div class="col-12 pb-40">
-            <h1 class="text-center fs-3 fw-bold text-primary">¿Por qué OnBase<sup>®</sup>?</h1>
+            <h1 class="text-center fs-3 fw-bold text-primary">Modelos Básicos</h1>
         </div>
-        <div class="col">
-            <div class="accordion" id="accordionFlushExample">
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#flush-collapseOne" aria-expanded="true" aria-controls="flush-collapseOne">
-                            Gestión Integral de Documentos
-                        </button>
-                    </h2>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse show"
-                        data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">OnBase<sup>®</sup> gestiona diversos tipos de documentos electrónicos,
-                            incluyendo imágenes, reportes, archivos de aplicación, formas HTML, mensajes de correo
-                            electrónico, y archivos multimedia.
-                        </div>
-                    </div>
+        <div class="row gx-md-2 mx-n24 custom-card">
+          <!-- Tarjetas de diferenciadores -->
+          <div class="col-md-8 d-none d-sm-block mx-auto">
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                            Ciclo de Vida Completo del Documento
-                        </button>
-                    </h2>
-                    <div id="flush-collapseTwo" class="accordion-collapse collapse"
-                        data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Desde la creación, almacenamiento, recuperación, revisión hasta la
-                            distribución de documentos.
-                        </div>
-                    </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Gestión Integral de Documentos</p>
                 </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#flush-collapseThree" aria-expanded="false"
-                            aria-controls="flush-collapseThree">
-                            Integración y Eficiencia
-                        </button>
-                    </h2>
-                    <div id="flush-collapseThree" class="accordion-collapse collapse"
-                        data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Utiliza una única aplicación basada en web con una base de datos SQL
-                            única, facilitando la integración de procesos y eliminando la necesidad de múltiples
-                            interfaces y bases de datos.</div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#flush-collapseFour" aria-expanded="false"
-                            aria-controls="flush-collapseFour">
-                            Escalabilidad y Rendimiento:
-                        </button>
-                    </h2>
-                    <div id="flush-collapseFour" class="accordion-collapse collapse"
-                        data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Desarrollada bajo una arquitectura cliente / servidor, aprovechando
-                            bases de datos robustas como Oracle y Microsoft SQL Server para garantizar alto desempeño y
-                            escalabilidad.
-                        </div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#flush-collapseFive" aria-expanded="false"
-                            aria-controls="flush-collapseFive">
-                            Acceso Versátil
-                        </button>
-                    </h2>
-                    <div id="flush-collapseFive" class="accordion-collapse collapse"
-                        data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Permite acceso desde navegadores web, clientes de escritorio,
-                            Windows Explorer<sup>®</sup> y otras aplicaciones.</div>
-                    </div>
-                </div>
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
-                            Configuración Sin Programación
-                        </button>
-                    </h2>
-                    <div id="flush-collapseSix" class="accordion-collapse collapse"
-                        data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Totalmente configurable a través de herramientas de interfaz
-                            gráfica, permitiendo implementaciones rápidas y adaptaciones sin costosas programaciones.
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Ciclo de Vida Completo del Documento</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Integración y Eficiencia</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Escalabilidad y Rendimiento</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Acceso Versátil</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-center">
+                <div class="col-1 align-items-center">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Configuración Sin Programación</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Movil : Columna -->
+          <div id="mobile-side-scroll" class="col d-sm-none">
+
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-top">
+                <div class="col-1 align-items-top">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Gestión Integral de Documentos</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-top">
+                <div class="col-1 align-items-top">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Ciclo de Vida Completo del Documento</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-top">
+                <div class="col-1 align-items-top">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Integración y Eficiencia</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-top">
+                <div class="col-1 align-items-top">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Escalabilidad y Rendimiento</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-top">
+                <div class="col-1 align-items-top">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Acceso Versátil</p>
+                </div>
+              </div>
+            </div>
+            <div class="card text-start mt-md-3">
+              <div class="row p-2 p-md-3 align-items-top">
+                <div class="col-1 align-items-top">
+                  <img class="checked-list" src="images/check-circle-fill.svg" alt="" />
+                </div>
+                <div class="col-11 align-items-center">
+                  <p class="mb-1 light-grey-text">Configuración Sin Programación</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Fin elementos móvil -->
         </div>
     </div>
 </div>
