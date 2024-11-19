97a4139f330ecd46a4b91ca8d957b7388ca4e45e
WebifyArg <mitrerod@gmail.com>
2024-11-19 15:44:39 -0300

    Filter White on Hover

--- templates/beneficios.php
+++ templates/beneficios.php
@@ -6,28 +6,28 @@
     </div>
     <div id="mobile-side-scroll" class="row">
       <div class="col-12 col-md-3">
-        <div class="p-4 bg-white rounded-3 h-100">
+        <div class="p-4 bg-white rounded-3 h-100 filter-white">
           <img class="mb-3" src="<?php $SERVER_URI ?>images/experiencia.svg" alt="">
           <h3 class="fs-6 text-primary text-bold">Experiencia</h3>
           <p>Tenemos una amplia experiencia en el mercado. 38 años sirviendo de manera exclusiva al mercado de ahorro y crédito.</p>
         </div>
       </div>
       <div class="col-12 col-md-3">
-        <div class="p-4 bg-white rounded-3 h-100">
+        <div class="p-4 bg-white rounded-3 h-100 filter-white">
           <img class="mb-3" src="<?php $SERVER_URI ?>images/unico-proveedor.svg" alt="">
           <h3 class="fs-6 text-primary text-bold">Centro de Servicio Local</h3>
           <p>Único proveedor para las cooperativas con un centro de llamadas de servicio al cliente en Puerto Rico.</p>
         </div>
       </div>
       <div class="col-12 col-md-3">
-        <div class="p-4 bg-white rounded-3 h-100">
+        <div class="p-4 bg-white rounded-3 h-100 filter-white">
           <img class="mb-3" src="<?php $SERVER_URI ?>images/cero-desventajas.svg" alt="">
           <h3 class="fs-6 text-primary text-bold">Continuo Desarrollo</h3>
           <p>Única institución de servicios que se ha mantenido desarrollando y educando al personal.</p>
         </div>
       </div>
       <div class="col-12 col-md-3">
-        <div class="p-4 bg-white rounded-3 h-100">
+        <div class="p-4 bg-white rounded-3 h-100 filter-white">
           <img class="mb-3" src="<?php $SERVER_URI ?>images/mantenimiento.svg" alt="">
           <h3 class="fs-6 text-primary text-bold">Mantenimiento</h3>
           <p>Realizar las mejoras al sistema a través de conexión remota para agilizar los procesos de los clientes.</p>
